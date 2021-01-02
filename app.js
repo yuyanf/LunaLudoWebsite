8cef064e895f099a1d57b833369296a55a0a9e34
Anton Vo <S300938@oslomet.no>
2021-01-02 21:42:36 +0100

    nav done except key + scroll

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const pawButton = document.querySelector(".pawButton");
 const sections = document.querySelectorAll("section");
 const burger = document.querySelector(".burger");
 const nav = document.querySelector(".nav-links");
+const navText = document.querySelectorAll(".nav-text");
+
+const idlePeriod = 100;
+const animationDuration = 1000;
+let lastAnimation = 0;
 let index = 0;
 
-const toggleNav = () => {
+function navClick() {
+  navText.forEach((text, i) => {
+    navText[i].addEventListener("click", () => {
+      setStates(i);
+      sections[i].scrollIntoView();
+
+      if (i >= sections.length - 1) {
+        pawButton.classList.add("pawButtonEnd");
+        index = -1;
+      } else {
+        pawButton.classList.remove("pawButtonEnd");
+      }
+    });
+  });
+}
+
+function toggleNav() {
   window.addEventListener("click", (event) => {
     if (event.target == nav || event.target.parentNode == nav) {
       return;
@@ -18,9 +39,9 @@ const toggleNav = () => {
       nav.classList.remove("nav-active");
     }
   });
-};
+}
 
-const goToPage = () => {
+function pawClick() {
   pawButton.addEventListener("click", () => {
     if (index == -1) pawButton.classList.remove("pawButtonEnd");
     index++;
@@ -32,12 +53,13 @@ const goToPage = () => {
     });
 
     setStates(index);
+
     if (index >= sections.length - 1) {
       pawButton.classList.add("pawButtonEnd");
       index = -1;
     }
   });
-};
+}
 
 function goToHome() {
   sections[0].scrollIntoView();
@@ -63,26 +85,7 @@ function setStates(aIndex) {
       break;
   }
 }
-// const setStates = (aIndex) => {
-//   index = aIndex;
-//   switch (aIndex) {
-//     case 0:
-//       window.history.pushState(null, null, "#home");
-//       break;
-//     case 1:
-//       window.history.pushState(null, null, "#story");
-//       break;
-//     case 2:
-//       window.history.pushState(null, null, "#stats");
-//       break;
-//     case 3:
-//       window.history.pushState(null, null, "#timeline");
-//       break;
-//     case 4:
-//       window.history.pushState(null, null, "#about");
-//       break;
-//   }
-// };
 
+navClick();
 toggleNav();
-goToPage();
+pawClick();
